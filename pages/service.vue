669d5746b02ceec0b7913ad2ca805fefda9917bf
maimalee <anasment6@gmail.com>
2024-05-21 15:47:46 +0100

    Fix: Redesign

--- pages/service.vue
+++ pages/service.vue
@@ -12,12 +12,12 @@
                         <div class="service-description">
                             <div>Transfer</div>
                             <div>This service enables users to send money to friends,
-                                 family, or businesses quickly and securely. 
-                                 Whether it's a local transfer or an international
-                                  remittance, our platform provides convenient options
-                                   for sending money with minimal fees. We prioritize security
-                                    and efficiency, ensuring that transfers are processed promptly and accurately. 
-                                    Join us to experience seamless money transfer solutions tailored to your needs.
+                                family, or businesses quickly and securely.
+                                Whether it's a local transfer or an international
+                                remittance, our platform provides convenient options
+                                for sending money with minimal fees. We prioritize security
+                                and efficiency, ensuring that transfers are processed promptly and accurately.
+                                Join us to experience seamless money transfer solutions tailored to your needs.
                             </div>
                         </div>
                     </div>
@@ -28,8 +28,8 @@
                             <div>
                                 This service allows users to purchase credit for their mobile phones
                                 , enabling them to make calls, send SMS, and use data. We ensure swift
-                                 and reliable airtime top-up options, offering various denominations to
-                                  cater to different user needs.
+                                and reliable airtime top-up options, offering various denominations to
+                                cater to different user needs.
                             </div>
                         </div>
                     </div>
@@ -39,10 +39,10 @@
                         <div class="service-description">
                             <div>Data Bundle</div>
                             <div>
-                                Users can purchase data bundles to access the internet on their mobile 
-                                devices. We provide a range of data packages, from small to large, suitable 
+                                Users can purchase data bundles to access the internet on their mobile
+                                devices. We provide a range of data packages, from small to large, suitable
                                 for different usage patterns. Our platform ensures seamless activation and
-                                 prompt delivery of data bundles.
+                                prompt delivery of data bundles.
                             </div>
                         </div>
                     </div>
@@ -52,14 +52,14 @@
                         <div class="service-description">
                             <div>TV Subscription</div>
                             <div>
-                                With this service, users can subscribe 
-                                to various television channels or streaming 
-                                services. We offer a diverse selection of 
+                                With this service, users can subscribe
+                                to various television channels or streaming
+                                services. We offer a diverse selection of
                                 subscription options, including both local
-                                 and international channels, catering to different 
-                                 entertainment preferences. Our platform ensures
-                                  hassle-free subscription management and timely 
-                                  renewal reminders.
+                                and international channels, catering to different
+                                entertainment preferences. Our platform ensures
+                                hassle-free subscription management and timely
+                                renewal reminders.
                             </div>
                         </div>
                     </div>
@@ -69,9 +69,9 @@
                         <div class="service-description">
                             <div>Exam Result</div>
                             <div>
-                                This service enables users to check their exam results online. We collaborate with 
+                                This service enables users to check their exam results online. We collaborate with
                                 educational institutions and examination boards to provide a secure and efficient
-                                 result checking process. Users can access their results quickly and conveniently 
+                                result checking process. Users can access their results quickly and conveniently
                                 through our platform, with appropriate privacy measures in place.
                             </div>
                         </div>
@@ -81,12 +81,12 @@
                         <img src="/Asset 35.png" alt="">
                         <div class="service-description">
                             <div>Electricity</div>
-                            <div>Users can conveniently pay their electricity 
-                                bills through our platform. We partner with utility 
+                            <div>Users can conveniently pay their electricity
+                                bills through our platform. We partner with utility
                                 providers to offer a seamless billing and payment experience.
-                                 Users can easily check their electricity consumption, receive
-                                  bill notifications, and make payments securely, helping them 
-                                  manage their energy usage efficiently.
+                                Users can easily check their electricity consumption, receive
+                                bill notifications, and make payments securely, helping them
+                                manage their energy usage efficiently.
                             </div>
                         </div>
                     </div>
@@ -100,18 +100,23 @@
 
 </script>
 <style scoped>
-.container-services{
+.container-services {
     width: 100%;
     font-family: Montserrat;
     justify-content: center;
     align-content: center;
-    margin: 70px auto;
-    /* background-image: url('/Asset 22.png'); */
+    margin: 50px auto;
+    background-image: url('/Asset 22.png');
     background-size: 400px;
     background-repeat: no-repeat;
     background-position: top right;
 
 }
+
+.services-main {
+    margin-top: 40px;
+}
+
 /* 
 .services-main-icon{
     display: flex;
@@ -129,25 +134,33 @@
 } */
 
 .services-main-icon {
-    display: flex; /* Make the container a flex container */
-    justify-content: space-around; /* Align the images horizontally */
-    align-items: center; /* Center the images vertically */
+    display: flex;
+    /* Make the container a flex container */
+    justify-content: space-around;
+    /* Align the images horizontally */
+    align-items: center;
+    /* Center the images vertically */
     width: 100%;
 }
 
 .services-main-icon img {
-    max-width: 45%; /* Ensure images don't exceed their natural size */
-    height: auto; /* Allow the height to adjust automatically */
+    max-width: 45%;
+    /* Ensure images don't exceed their natural size */
+    height: auto;
+    /* Allow the height to adjust automatically */
+    margin-top: 40px;
 }
 
-.services-main-icon :last-child{
+.services-main-icon :last-child {
     width: 25%;
-} 
+}
 
 .services-list {
-    display: grid; /* Changed from flex to grid */
+    display: grid;
+    /* Changed from flex to grid */
     grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
-    gap: 50px; /* Adjust the gap between service items */
+    gap: 50px;
+    /* Adjust the gap between service items */
     width: 90%;
     margin: 20px auto;
 }
@@ -155,56 +168,67 @@
 .service {
     display: flex;
     flex-direction: column;
-    align-items: center  ;
+    align-items: center;
     text-align: justify;
     background-color: #FAFBFF;
     padding: 25px;
     border-radius: 10px;
 }
 
-.service-description :first-child{
+.service-description :first-child {
     font-size: 20px;
     font-weight: bolder;
     color: #f17540;
 }
-@media screen and (max-width: 830px) {
-    .container-services{
-    width: 87%;
 
-}
+@media screen and (max-width: 830px) {
+    .container-services {
+        margin: 0px auto;
+        background-size: 100px;
+    }
+    .services-main{
+        width: 85%;
+        margin: 20px auto;
+    }
     .services-list {
         display: grid;
-        grid-template-columns: repeat(1, 1fr); /* Two services in each row */
-        gap: 10px; /* Adjust the gap between service items */
+        grid-template-columns: repeat(1, 1fr);
+        /* Two services in each row */
+        gap: 10px;
+        /* Adjust the gap between service items */
         width: 100%;
     }
-    .services-main-icon :first-child{
+
+    .services-main-icon :first-child {
         max-width: 100%;
     }
-    .services-main-icon :last-child{
+
+    .services-main-icon :last-child {
         display: none;
     }
+
     .service {
         display: flex;
         flex-direction: column;
         align-content: center;
-        padding:10px;
+        padding: 10px;
         border-radius: 10px;
         font-size: 14px;
     }
-    .services-list :nth-child(4) img{
-        margin-top:15px;
+
+    .services-list :nth-child(4) img {
+        margin-top: 15px;
         margin-bottom: 30px;
     }
-    .service img{
+
+    .service img {
         width: 50px;
         margin-bottom: 20px;
-        
+
     }
 
-.service-description :first-child{
-    font-size: 14px;
+    .service-description :first-child {
+        font-size: 14px;
+    }
 }
-}
-
 </style>
